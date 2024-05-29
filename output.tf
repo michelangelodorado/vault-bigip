bc5f55d35aeaf7bab3bad6d0d7c6b095f8bfc0bb
Mike <102953584+michelangelodorado@users.noreply.github.com>
2024-05-30 00:05:28 +0800

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,15 @@
+output "f5_ui" {
+  value = "https://${aws_eip.f5.public_ip}:8443"
+}
+
+output "vault_ui" {
+  value = "http://${aws_instance.vault.public_ip}:8200"
+}
+
+output "F5_Password" {
+  value = random_string.password.result
+}
+
+output "To_SSH_into_vault_ubuntu" {
+  value = "ssh -i ${aws_key_pair.demo.key_name}.pem ubuntu@${aws_instance.vault.public_ip}"
+}
