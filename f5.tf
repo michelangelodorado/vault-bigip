e3752caeb85ce4bb62810e15d3407576d38b3da6
Mike <102953584+michelangelodorado@users.noreply.github.com>
2024-05-31 15:19:13 +0800

    Update f5.tf

--- f5.tf
+++ f5.tf
@@ -4,8 +4,8 @@ resource "random_string" "password" {
 }
 
 resource "aws_instance" "f5" {
-  # F5 BIGIP-14.1.0.3-0.0.6 PAYG-Good 25Mbps-190326002717
-  ami = "ami-00a9fd893d5d15cf6"
+  # F5 BIGIP-17.1.1.3-0.0.5 PAYG-Good 25Mbps
+  ami = "ami-046a61b731e6de3a5"
 
   instance_type               = "m5.xlarge"
   private_ip                  = "10.0.0.200"
